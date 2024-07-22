15fb4742681e4f9155b443e9fc937dea4788ceb5
Jeremiah Faluyi <faluyijeremiah2018@gmail.com>
2024-07-22 20:13:27 +0100

    removed un-needed code

--- utils/config.ts
+++ utils/config.ts
@@ -9,30 +9,10 @@ export class Config {
 	private static metadata_error_message = "";
 
 	static async getAppMetadata(): Promise<AppMetadata> {
-		try {
-			if (this.APP_METADATA) return this.APP_METADATA;
-
-			const _metadata = (await getFirestore().collection(env.APP_VERSION).doc("config").get()).data() as AppMetadata
-
-			if (!_metadata) {
-				this.metadata_error_message = "unable to retrieve dependent information";
-				throw new InternalServerErrorException();
-			}
-			await this.setAppMetadata(_metadata);
-
-			return this.APP_METADATA
-		} catch (e) {
-			this.logger.error(e);
-			throw new InternalServerErrorException(this.metadata_error_message);
-		}
+		
 	}
 
 	static async setAppMetadata(data: AppMetadata) {
-		if (!data['BRANDING_PACKS'] || !data['BUSINESS_REG_AMOUNT'] || !data['COMPANY_REG_AMOUNT'] || !data['INCORPORATION_AMOUNT'] || !data['CORPORATE_ACCOUNT_AMOUNT']) {
-			this.metadata_error_message = "failed to resolve app dependency";
-			throw new InternalServerErrorException(this.metadata_error_message);
-		}
-
-		this.APP_METADATA = data;
+		
 	}
 }
